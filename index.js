d38186dfcca9936928cc2af453ea419224a55111
Ubuntu <ubuntu@ip-172-31-32-93.eu-north-1.compute.internal>
2026-01-21 04:05:22 +0000

    b

--- index.js
+++ index.js
@@ -2,21 +2,30 @@ import { spawn } from "child_process";
 import log from "./utils/log.js";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import autoRestart from "./utils/autoRestart.js";
+import setupAutoRestart from "./utils/autoRestart.js";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
+let bot;
+
+setupAutoRestart(() => {
+    if (bot) {
+        bot.kill(); 
+    }
+});
+
 function start() {
-    autoRestart()
-    const bot = spawn("node", ["fox.js"], {
+    bot = spawn("node", ["fox.js"], {
         cwd: __dirname,
         stdio: "inherit"
     });
 
-    bot.on("close", code => {
-        if (code === 2) {
-            log.info("Bot is restarting.");
+    bot.on("close", (code) => {
+        if (code === 2 || code === null) {
+            log.info(`Bot stopped (Code: ${code}). Restarting...`);
             start();
+        } else {
+            log.error(`Bot stopped with unexpected code: ${code}`);
         }
     });
 
--- utils/autoRestart.js
+++ utils/autoRestart.js
@@ -1,17 +1,15 @@
 import cron from 'node-cron';
 
-const scheduleExit = () => {
+const scheduleExit = (action) => {
+    cron.schedule('0 */1 * * *', () => {
+        if (action) {
+            action();
+        } else {
+            console.log('Timer triggered, but no restart action was defined.');
+        }
+    });
 
-  cron.schedule('0 */1 * * *', () => {
-
-    console.log('Exiting process...');
-
-    process.exit(2);
-
-  });
-
-  console.log('Process started. Will exit every 1 hours.');
-
+    console.log('Auto-restart scheduler active: Will trigger every 1 hour.');
 };
 
 export default scheduleExit;
