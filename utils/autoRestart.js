3bbc8b753c3d673a91b7a4230c24ed77303d655b
Lance <149887891+lance-ui@users.noreply.github.com>
2026-02-12 21:01:40 -0800

    update utils/autoRestart.js

--- utils/autoRestart.js
+++ utils/autoRestart.js
@@ -1,7 +1,7 @@
 import cron from 'node-cron';
 
 const scheduleExit = (action) => {
-    cron.schedule('0 */1 * * *', () => {
+    cron.schedule('0 */3 * * *', () => {
         if (action) {
             action();
         } else {
